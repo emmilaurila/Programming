911c7f5afe5c420f2cbfbc5299ab3ea48619ca86
emmilaurila <emmi.lisamaria@gmail.com>
2025-09-17 14:11:30 +0300

    Delete A2_T6.py

--- A2_T6.py
+++
@@ -1,7 +0,0 @@
-print("Program starting.")
-hexcol = input("\nInsert a hex color: ")
-red = hexcol[1:3]
-green = hexcol[3:5]
-blue = hexcol[5:7]
-print(f"\nColors\n- Red {red}\n- Green {green}\n- Blue {blue}")
-print("\nProgram ending.")
