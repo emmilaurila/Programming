5263cfc293c19a3ae383bc25e4318fe671776826
emmilaurila <emmi.lisamaria@gmail.com>
2025-09-17 14:10:22 +0300

    Add files via upload

---
+++ week 2/A2_T2.py
@@ -0,0 +1,8 @@
+print("Program staring.")
+brand = input("Insert car brand: ")
+model = input("Insert car model: ")
+print("Car brand is", f'"{brand}" ', end="")
+print(f"and the model is", f"'{model}'.", sep=" ")
+print("Program ending.")
+
+
